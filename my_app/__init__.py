9ef3b165ca0687eee1ccc55b42ad6b9f4cf09b27
charles-42 <71760090+charles-42@users.noreply.github.com>
2022-07-06 15:29:50 +0200

    add opentelemetry

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -4,10 +4,29 @@ from .models import init_db
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace import config_integration
+from opencensus.ext.azure import metrics_exporter
+
+import os
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
 def create_app(mode = "development"):
-    load_dotenv()
+    
+    # Upload env variables
+    load_dotenv(override=True)
+    
+    # Create APP
     app = Flask(__name__, instance_relative_config=True)
     
+    # Select config
     if mode  == "development":
         app.config.from_object('config.DevelopmentConfig')
     elif mode == "test":
@@ -15,10 +34,11 @@ def create_app(mode = "development"):
     elif mode == "production":
         app.config.from_object('config.ProductionConfig')
 
-    #from my_app.models import db
 
+    # Connect to DB
     db = SQLAlchemy(app)
 
+    # Connoct to log manager with flask login
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -28,15 +48,15 @@ def create_app(mode = "development"):
         # since the id is just the primary key of our user table, use it in the query for the user
         return models.User.query.get(int(id))
 
+
+    # Define cli command to initiate DB
     from my_app import models
-
-
 
     @app.cli.command("init_db")
     def init_db():
         models.init_db()
 
-
+    # Define routes
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
@@ -44,6 +64,33 @@ def create_app(mode = "development"):
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+
+    # Define monitoring
+    middleware = FlaskMiddleware(app)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(AzureLogHandler(
+    connection_string = os.getenv('CONNEXION_STRING'))
+    )
+    logger.setLevel(logging.INFO)
+    config_integration.trace_integrations(['sqlalchemy'])
+
+    exporter = metrics_exporter.new_metrics_exporter(
+        enable_standard_metrics=False,
+        connection_string=os.getenv('CONNEXION_STRING'))
+
+
+    # OPentelemetry
+
+    exporter = AzureMonitorTraceExporter.from_connection_string(os.getenv('CONNEXION_STRING')
+    )
+
+    trace.set_tracer_provider(TracerProvider())
+    tracer = trace.get_tracer(__name__)
+    span_processor = BatchSpanProcessor(exporter)
+    trace.get_tracer_provider().add_span_processor(span_processor)
+    FlaskInstrumentor().instrument_app(app)
+
 
     return app
 
--- my_app/auth.py
+++ my_app/auth.py
@@ -13,6 +13,8 @@ def login():
 @auth.route('/login', methods=['POST'])
 def login_post():
     # login code goes here
+    current_app.logger.info("Login attempt", extra = {'page':'login'})
+   
     email = request.form.get('email')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
@@ -23,10 +25,13 @@ def login_post():
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
+        current_app.logger.warning("Login failed", extra = {'page':'login'})
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
+
+    current_app.logger.info("Login succes", extra = {'page':'login'})
 
     return redirect(url_for('main.profile'))
 
